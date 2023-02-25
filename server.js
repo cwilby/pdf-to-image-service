adaa216adcfdc1b8bbb79f4cbd649d5154e37a3a
Cameron Wilby <cameron@wilby.com>
2023-02-25 03:44:27 -0800

    Updates

--- server.js
+++ server.js
@@ -7,23 +7,26 @@ const multer = require('multer');
 const path = require('path');
 const PDFBox = require("pdfbox-simple");
 
+const uploadDirectory = path.resolve(__dirname, 'uploads');
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination: (req, file, done) => done(null, 'uploads/'),
+		filename: (req, file, done) => done(null, `${Date.now()}${path.extname(file.originalname)}`)
+	})
+});
+
 const app = express();
 
 app.use(morgan('combined'));
 
-const storage = multer.diskStorage({
-	destination: (req, file, done) => done(null, 'uploads/'),
-	filename: (req, file, done) => done(null, `${Date.now()}${path.extname(file.originalname)}`)
-});
-
-const upload = multer({ storage });
-
-app.get('/health', (req, res) => res.sendStatus(200));
-
 app.post('/pdf-to-jpg', upload.single('pdf'), async (req, res) => {
-	const uploadDirectory = path.resolve(__dirname, 'uploads');
-	const pdfPath = path.resolve(uploadDirectory, req.file.filename);
+	res.on('finish', () => fs.readdirSync(uploadDirectory).forEach(filename => {
+		if (filename === '.gitignore') return;
+		fs.rmSync(path.resolve(uploadDirectory, filename));
+	}));
 
+	const pdfPath = path.resolve(uploadDirectory, req.file.filename);
 	await new PDFBox().exec("PDFToImage", pdfPath);
 
 	fs.rmSync(pdfPath);
@@ -32,22 +35,13 @@ app.post('/pdf-to-jpg', upload.single('pdf'), async (req, res) => {
 	res.attachment(`${Date.now()}.zip`);
 	archive.on('error', () => res.sendStatus(500));
 	archive.pipe(res);
-
-	for (const pageImage of fs.readdirSync(uploadDirectory)) {
-		if (pageImage === '.gitignore') continue;
-
-		archive.file(path.resolve(uploadDirectory, pageImage), { name: path.basename(pageImage) });
-	}
-
-	archive.finalize();
-
-	res.on('finish', () => {
-		for (const file of fs.readdirSync(uploadDirectory)) {
-			if (file === '.gitignore') continue;
-
-			fs.rmSync(path.resolve(uploadDirectory, file));
-		}
+	fs.readdirSync(uploadDirectory).filter(f => f !== '.gitignore').forEach((filename, index) => {
+		if (filename === '.gitignore') return;
+		archive.file(path.resolve(uploadDirectory, filename), { name: `${index}.jpg` });
 	});
+	archive.finalize();
 });
+
+app.get('/health', (req, res) => res.sendStatus(200));
 
 app.listen(8000);
